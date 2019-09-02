98d7cd5c8dd5d0b487ae01f4b78a359332bcf452
Aleksey Timchenko <flegmatik.bel@gmail.com>
2019-09-02 16:57:45 +0300

    add CI env

--- vars/setupHostEnv.groovy
+++ vars/setupHostEnv.groovy
@@ -3,4 +3,6 @@ def call() {
   env.HOST_UID = sh(script: 'echo "$(id -u "$USER")"', returnStdout: true).trim()
   env.HOST_USERNAME = env.USER
   env.HOST_GROUPNAME = sh(script: 'echo "$(id -ng "$USER")"', returnStdout: true).trim()
+  
+  env.CI = '1'
 }
